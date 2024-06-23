eb345c0775b012ba43b135df3ebaa6aeb0d235bc
Vinh Tan Nguyen <nguyentanvinh2911@gmail.com>
2024-06-23 22:17:58 +0700

    Initial commit

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello world")
     }
 
-
+    @IBAction func changeClicked(_ sender: Any) {
+        imageView.image = UIImage(named: "metallica2")
+    }
+    
 }
 
